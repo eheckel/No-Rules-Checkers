2df44bde8ba7942c0c7c3bcd21f588bda7ee100e
Emily Heckel <eheckel@gmail.com>
2018-08-29 18:52:13 -0400

    First commit

---
+++ src/comp110/CheckersApp.java
@@ -0,0 +1,16 @@
+package comp110;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CheckersApp extends Application {
+
+	public static void main(String[] args) {
+		Application.launch();
+	}
+
+	public void start(Stage s) {
+		new CheckersWindow();
+	}
+
+}
